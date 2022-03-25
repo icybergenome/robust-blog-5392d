46dfcb6db5a66f0f85c5856eff9e8c902d70f530
sikander786 <sikander@useevs.com>
2022-03-26 01:37:41 +0500

    queries for page and header

--- src/utils/graphql/queries/get.js
+++ src/utils/graphql/queries/get.js
@@ -53,15 +53,79 @@ query posts {
     }
 }
 `;
-// export const getPosts =() => {
-//     // let posts = {}
-//     const {data,error,loading} = useQuery(GET_POSTS);
-//     return  data;
-// }
+
+export const GET_HEADER = gql`
+    query header {
+        header {
+            id
+            created_at
+            updated_at
+            title
+            tagline
+            logo_img {
+                url
+                alternativeText
+            }
+            background_img {
+                url
+                alternativeText
+            }
+            has_nav
+            nav_links {
+                ... on ComponentSharedNavLinks {
+                    label
+                    url
+                    style
+                }
+            }
+            has_social
+            social_links {
+                ... on ComponentSharedSocialLinks {
+                    label
+                    url
+                    style
+                    icon_class
+                    new_window
+                }
+            }
+        }
+    }
+`;
+
+
+export const GET_PAGE = gql`
+    query page($id: ID!) {
+        page(id: $id) {
+            id
+            created_at
+            updated_at
+            title
+            subtitle
+            img_path {
+                url
+            }
+            layout
+            model_name
+            seo {
+                title
+                description
+                extra {
+                    name
+                    value
+                    keyName
+                    relativeUrl
+                }
+            }
+            
+            }
+        
+    }
+    `;
+
     
-export const getCategories = () => {
-    let categories = {}
-    const {data,error,loading} = useQuery(GET_CATEGORIES);
-    return categories = data;
-};
+// export const getCategories = () => {
+//     let categories = {}
+//     const {data,error,loading} = useQuery(GET_CATEGORIES);
+//     return categories = data;
+// };
 
