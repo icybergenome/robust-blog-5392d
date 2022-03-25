995217dfedde36f26deef51bedae407767366dd3
sikander786 <sikander@useevs.com>
2022-03-26 01:31:11 +0500

    updated page

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -5,20 +5,23 @@ import { withRemoteDataUpdates } from 'sourcebit-target-next/with-remote-data-up
 import { getPage } from '../utils/data/getPage';
 import { data } from '../../content/idMaping/types';
 import { getHeader, getCategories } from '../utils/data/getConfigurations';
-import { getPosts } from '../utils/data/getPosts';
+import { GET_POSTS } from '../utils/graphql/queries/get';
+import { useQuery } from '@apollo/client';
 import pageLayouts from '../layouts';
 
-class Page extends React.Component {
-    render() {
-        const modelName = _.get(this.props.page, 'page.model_name');
-        // const modelName = _.get(this.props, 'page.__metadata.modelName');
+const Page = (props) => {
+    
 
-        const PageLayout = pageLayouts[modelName];
-        if (!PageLayout) {
-            throw new Error(`no page layout matching the page model: ${modelName}`);
-        }
-        return <PageLayout {...this.props} />;
+    const {data,error,loading} = useQuery(GET_POSTS);
+    const modelName = _.get(props.page, 'page.model_name');
+
+    const PageLayout = pageLayouts[modelName];
+    if (!PageLayout) {
+        throw new Error(`no page layout matching the page model: ${modelName}`);
     }
+    
+    return <PageLayout {...props} posts={data}/>;
+    
 }
 
 export async function getStaticPaths() {
@@ -27,16 +30,15 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    // const pagePath = '/' + (params.slug ? params.slug.join('/') : '');
-    // const props = await sourcebitDataClient.getStaticPropsForPageAtPath(pagePath);
     const slug = data.find((d) => d.name === (params.slug ? params.slug[0] : '/'));
     const props = {
+        slug: slug.name,
         page: await getPage({ id: slug.id, fields: slug.fields }),
         header: await getHeader(),
-        posts: await getPosts(),
-        categories: await getCategories()
+        categories: await getCategories(),
+        protected: true
     };
     return { props };
 }
 
-export default withRemoteDataUpdates(Page);
+export default Page;
