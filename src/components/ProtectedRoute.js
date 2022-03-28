5d9b93fb782cddffe87491959603ee7ac0042323
sikander786 <sikander@useevs.com>
2022-03-29 00:08:24 +0500

    updated protected routes

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Login from './Login'
 
 import { useContext } from 'react';
@@ -6,64 +6,57 @@ import { AuthContext } from '../utils/context/auth-context';
 import { useRouter } from 'next/router';
 import pageLayouts from '../layouts';
 import { useQuery } from '@apollo/client';
-import { GET_POSTS } from '../utils/graphql/queries/get';
-import { GET_CATEGORIES } from '../utils/graphql/queries/get';
-import { GET_HEADER } from '../utils/graphql/queries/get';
-import { GET_PAGE } from '../utils/graphql/queries/get';
+import { GET_POSTS } from '../utils/graphql/queries/getPosts';
+import { GET_CATEGORIES } from '../utils/graphql/queries/getCategories';
+import { GET_HEADER } from '../utils/graphql/queries/getHeader';
+import { GET_PAGE } from '../utils/graphql/queries/getPage';
 
 import { data } from '../../content/idMaping/types';
-
 import _ from 'lodash';
+import { css } from "@emotion/react";
+
+import LoadingSpinner from './LoadingSpinner';
+
 
 export default function ProtectedRoute(props) {
-    
-    const router = useRouter()
-    const authContext = useContext(AuthContext);
-    
-    const Component = props.element
-    
+    const [isLoading, setIsLoading] = useState(false) 
+    const [error, setIsError] = useState(false) 
 
+    const router = useRouter()
+    const { pathname } = router;
+  
+    const authContext = useContext(AuthContext);
     const auth = authContext.isUserAuthenticated();
 
-    console.log("My Protected AUTH",auth)
+    const Component = props.element
 
-    const queryMultiple = () => {
-      const slug = data.find((d) => d.name === '/');
+    useEffect(() => {
 
-      const res1 = useQuery(GET_CATEGORIES)
-      const res2 = useQuery(GET_HEADER)
-      const res3 = useQuery(GET_POSTS)
-      const res4 = useQuery(GET_PAGE,{variables: {id:slug.id}})
-      return [res1,res2,res3,res4];
-    }
-    const resultQuery = queryMultiple()
-    
-
-
-    if(!auth && props.componentProps.slug !== 'login'){
-        // router.replace("/login")
-        return <Login />
-    }
-
-    if(auth && (props.componentProps.slug === 'login' || props.componentProps.slug == 'register')){
-      const categories = resultQuery[0].data;
-      const header = resultQuery[1].data;
-      const post = resultQuery[2].data;
-      const page = resultQuery[3].data;
-
-      const page_pros = {
-        categories: categories,
-        header: header,
-        page: page,
+      console.log("authhhhhh",auth)
+      console.log("slug",props.componentProps.slug)
+      const token = localStorage.getItem("jwtToken")
+      if(!token && (props.componentProps.slug !== 'login' && props.componentProps.slug !== 'register') ){
+        console.log("In login")
+        setIsLoading(false)
+        router.push('/login')
+        return
       }
 
-      const Home = pageLayouts['home']
-        
-        // router.push('/login', '/', { shallow: true })
-        return <Home {...page_pros} posts={post}/>;
-    }
+      if(token && (props.componentProps.slug === 'login' && props.componentProps.slug === 'register')){
+        console.log("In Home")
+        setIsLoading(false)
+        router.push('/')
+        return
+      }
+
+    }, [auth])
+    
 
    return (
-    <Component {...props.componentProps}/>
+     
+    <>
+      {isLoading ? <LoadingSpinner isLoading={isLoading}/> : <Component {...props.componentProps}/>} 
+      
+    </>
   )
 }
