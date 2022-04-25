a57b70ed198eb497725968e4ca3f88f4e0d19929
sikander786 <sikander@useevs.com>
2022-04-26 02:22:42 +0500

    little modification

--- src/utils/apollo/apollo.js
+++ src/utils/apollo/apollo.js
@@ -6,26 +6,36 @@ import {
 
 import { setContext } from '@apollo/client/link/context';
 import { isValid } from "../helpers/isValid";
+import { useContext } from "react";
+
+import { AuthContext } from "../context/auth-context";
+
+
+// let token;
+
+// custom module setter:
+// export const setToken = (newToken) => token = newToken;
+
 
 
 const httpLink = createHttpLink({
   uri: `http://localhost:1337/graphql`,
 });
 
+ 
 
 const authLink = setContext((_, { headers }) => {
+  
   let token = ''
   try{
     token = localStorage.getItem('jwtToken');
+
     // const userInfo = localStorage.getItem("userInfo")
     // const valid = isValid(token)
   }catch(e){
     console.log(e.message)
   }
-
   // token = valid.valid && valid.user_id == userInfo.id ? token : ''
-
-  console.log("Apollo token",token)
   return {
     headers: {
       ...headers,
