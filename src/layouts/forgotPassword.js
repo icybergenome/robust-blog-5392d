967d97e663fff9c1e0ec91b09957d40571e70c62
sikander786 <sikander@useevs.com>
2022-04-26 01:58:29 +0500

    dynamic page forgot password layout

---
+++ src/layouts/forgotPassword.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+import ForgotPassord from '../components/ForgotPassord'
+
+const MyForgotPassord = () => {
+  return (
+    <ForgotPassord />
+  )
+}
+
+export default MyForgotPassord;
