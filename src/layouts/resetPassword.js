879ec3127d4af965438e95d6c087ea4e77cf028b
sikander786 <sikander@useevs.com>
2022-04-26 02:01:10 +0500

    dynamic page reset password layout

---
+++ src/layouts/resetPassword.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ResetPassword from '../components/ResetPassword'
+
+const MyResetPassword = () => {
+  return (
+    <ResetPassword />
+  )
+}
+
+
+export default MyResetPassword;
