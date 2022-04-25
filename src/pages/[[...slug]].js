56255d25ab95122c0a156103c690eaaa93fa6ae4
sikander786 <sikander@useevs.com>
2022-04-26 02:25:26 +0500

    updated queries

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,41 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import _ from 'lodash';
 import { sourcebitDataClient } from 'sourcebit-target-next';
 import { withRemoteDataUpdates } from 'sourcebit-target-next/with-remote-data-updates';
-import { getPage } from '../utils/data/getPage';
 import { data } from '../../content/idMaping/types';
-import { getHeader, getCategories } from '../utils/data/getConfigurations';
 import { GET_POSTS } from '../utils/graphql/queries/getPosts';
-import { useQuery } from '@apollo/client';
+import { GET_CATEGORIES } from '../utils/graphql/queries/getCategories';
+import { GET_PAGE } from '../utils/graphql/queries/getPage';
+import { GET_HEADER } from '../utils/graphql/queries/getHeader';
+
 import pageLayouts from '../layouts';
 
-const Page = (props) => {
-    
+import { client } from '../utils/apollo/apollo';
 
-    const {data,error,loading} = useQuery(GET_POSTS);
+
+const Page = (props) => {
+
     const modelName = _.get(props.page, 'page.model_name');
 
     const PageLayout = pageLayouts[modelName];
     if (!PageLayout) {
         throw new Error(`no page layout matching the page model: ${modelName}`);
     }
+
+
     
-    return <PageLayout {...props} posts={data}/>;
+    return <PageLayout {...props} />;
     
 }
 
 export async function getStaticPaths() {
     const paths = await sourcebitDataClient.getStaticPaths();
     return { paths, fallback: false };
+    
 }
 
-export async function getStaticProps({ params }) {
-    const slug = data.find((d) => d.name === (params.slug ? params.slug[0] : '/'));
+export async function getStaticProps({params}) {
+    const slug = data.find((d) => d.name === (params.slug ? params.slug[0] : '/'))
+    const categories = await client.query({
+        query: GET_CATEGORIES,
+      });
+    const page = await client.query({
+        query: GET_PAGE,
+        variables:{
+            id: slug.id,
+            fields: slug.fields
+        }
+      });
+    const header = await client.query({
+        query: GET_HEADER,
+      });
+    const posts = await client.query({
+        query: GET_POSTS,
+      });
+    
+    
     const props = {
         slug: slug.name,
-        page: await getPage({ id: slug.id, fields: slug.fields }),
-        header: await getHeader(),
-        categories: await getCategories(),
+        page: page.data,
+        header: header.data,
+        categories: categories.data,
+        posts: posts.data,
         protected: true
     };
     return { props };
