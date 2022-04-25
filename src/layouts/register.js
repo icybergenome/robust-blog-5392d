c96e44d79c9de3d4abcb8d771aa8f56dbf54f606
sikander786 <sikander@useevs.com>
2022-04-26 02:01:40 +0500

    dynamic page register layout

---
+++ src/layouts/register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from '../components/Register'
+
+const MyRegister = () => {
+  return (
+    <>
+        <Register />
+    </>
+    
+  )
+}
+
+export default MyRegister;
