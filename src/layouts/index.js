61f2ef16dce477d83a9fe10eb48c7dfea50aad81
sikander786 <sikander@useevs.com>
2022-04-26 02:26:03 +0500

    export layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,12 +2,22 @@ import contact from './contact';
 import home from './home';
 import page from './page';
 import post from './post';
+import newPost from './newPost';
+import register from './register';
+import login from './login'
+import forgotPassword from './forgotPassword'
+import resetPassword from './resetPassword';
 
-export { contact, home, page, post };
+export { contact, home, page, post, newPost, register, login, forgotPassword, resetPassword };
 
 export default {
     contact,
     home,
     page,
-    post
+    post,
+    newPost,
+    register,
+    login,
+    forgotPassword,
+    resetPassword
 };
