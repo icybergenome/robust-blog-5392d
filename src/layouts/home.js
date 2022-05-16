ddfb8772467f00cc5fbc8acad9d409142922ea8f
sikander786 <sikander@useevs.com>
2022-05-16 15:15:19 +0500

    updated home layout

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -6,17 +6,24 @@ import { Layout } from '../components/index';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { getPageUrl, Link, withPrefix } from '../utils';
-import { post } from '.';
-export default class Home extends React.Component {
-    renderPost(post, index, hasMoreLink, moreLinkText) {
+import { useCMS } from 'tinacms';
+// import { post } from '.';
+const Home = (props) => {
+    // const cms = useCMS()
+    // cms.disable()
+    // cms.sidebar.isOpen = false
+
+    const renderPost = (post, index, hasMoreLink, moreLinkText) => {
         const title = _.get(post, 'title');
         const thumbImage = getStrapiMedia(_.get(post, 'cover_img'));
         const thumbImageAlt = _.get(post, 'cover_img.alternativeText', '');
+        const post_img_url = _.get(post, 'post_img_url');
         const excerpt = _.get(post, 'subtitle');
         const date = _.get(post, 'date');
         const dateTimeAttr = moment(date).strftime('%Y-%m-%d %H:%M');
         const formattedDate = moment(date).strftime('%B %d, %Y');
         const postUrl = getPageUrl(post, { withPrefix: true });
+         
         return (
             <article key={index} className="post">
                 <header className="post-header">
@@ -30,45 +37,44 @@ export default class Home extends React.Component {
                         </time>
                     </div>
                 </header>
-                {thumbImage && (
-                    <Link className="post-thumbnail" href={postUrl}>
-                        <img className="thumbnail" src={withPrefix(thumbImage)} alt={thumbImageAlt} />
-                    </Link>
-                )}
+                
+                <Link className="post-thumbnail" href={postUrl}>
+                    <img className="thumbnail" src={post_img_url ? post_img_url : '/images/no-image.jpg'} />
+                </Link>
+                
                 {excerpt && (
                     <div className="post-content">
                         <p>{excerpt}</p>
                     </div>
                 )}
-                {hasMoreLink && moreLinkText && (
-                    <p className="read-more">
-                        <Link className="read-more-link" href={postUrl}>
-                            {moreLinkText} <span className="icon-arrow-right" aria-hidden="true" />
-                        </Link>
-                    </p>
-                )}
+                
+                <p className="read-more">
+                    <Link className="read-more-link" href={postUrl}>
+                        Keep Reading {moreLinkText} <span className="icon-arrow-right" aria-hidden="true" />
+                    </Link>
+                </p>
+                
             </article>
         );
     }
 
-    render() {
-        const data = _.get(this.props, 'data');
+        const data = _.get(props, 'data');
         const config = _.get(data, 'config');
-        const header = _.get(this.props, 'header.header');
+        const header = _.get(props, 'header.header');
         const headerImage = getStrapiMedia(_.get(header, 'background_img'));
-        const page = _.get(this.props, 'page.page');
+        const page = _.get(props, 'page.page');
         const hasMoreLink = _.get(page, 'page.has_more_link');
-        const categories = _.get(this.props, 'categories.categories');
+        const categories = _.get(props, 'categories.categories');
 
         const moreLinkText = _.get(page, 'page.more_link_text');
-        const posts = _.orderBy(_.get(this.props, 'posts', []), 'published_at', 'desc');
+        const posts = _.orderBy(_.get(props, 'posts', []), 'published_at', 'desc');
         return (
             <Layout page={page} config={config}>
-                <Header config={config} page={this.props} image={headerImage} />
+                <Header config={config} page={props} image={headerImage} />
                 <div id="content" className="site-content">
                     <div style={{ display: 'flex', flexDirection: 'row' }}>
                         <main id="main" className="site-main inner">
-                            <div className="post-feed">{_.map(posts[0], (post, index) => this.renderPost(post, index, hasMoreLink, moreLinkText))}</div>
+                            <div className="post-feed">{_.map(posts[0], (post, index) => renderPost(post, index, hasMoreLink, moreLinkText))}</div>
                         </main>
                         <div className="categories_box">
                             <h4>Categories</h4>
@@ -90,5 +96,8 @@ export default class Home extends React.Component {
                 </div>
             </Layout>
         );
-    }
+    
 }
+
+
+export default Home;
