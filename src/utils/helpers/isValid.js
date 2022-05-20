a0f55caaad227c4a05a3572cfabc65b9663e10db
sikander786 <sikander@useevs.com>
2022-05-20 19:18:32 +0500

    updated helper

--- src/utils/helpers/isValid.js
+++ src/utils/helpers/isValid.js
@@ -1,7 +1,9 @@
 import jwt_decode from "jwt-decode";
+import { Jwt } from "jsonwebtoken";
+
 
 export const isValid = (jwt) => {
-
+    // const d_json = Jwt.verify()
         const decoded = jwt_decode(jwt);
         const date = Date.now()
         if (date >= decoded.exp * 1000) {
